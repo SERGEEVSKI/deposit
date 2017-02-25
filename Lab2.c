f6cf9d18d5b5d318410b05333db08523224d923b
Arthur Sergeev <sergeevski@outlook.com>
2017-02-25 23:06:49 +0700

    Implement data validation

--- Lab2.c
+++ Lab2.c
@@ -7,5 +7,15 @@ int main()
 	scanf("%d", &sum);
 	printf("\nInsert days, please: ");
 	scanf("%d", &days);
+	while(sum<10000)
+	{
+		printf("\nFailed! Insert please sum not less than 10,000: ");
+		scanf("%d", &sum );
+	}
+	while(days>365)
+	{
+		printf("\nFailed! Insert please days no more than 365: ");
+		scanf("%d", &days );
+	}
 
 }
