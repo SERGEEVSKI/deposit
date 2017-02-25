4b96408eb92471b37cdf9aa23c1e5fa26a448da7
Arthur Sergeev <sergeevski@outlook.com>
2017-02-25 23:09:08 +0700

    Implement the algorithm of counting the contribution for summ<=100000

--- Lab2.c
+++ Lab2.c
@@ -17,5 +17,16 @@ int main()
 		printf("\nFailed! Insert please days no more than 365: ");
 		scanf("%d", &days );
 	}
+	if(sum<=100000)
+	{
+		if((days>=0)&&(days<=30))
+			res=sum-(sum/100*10);
+		if((days>=31)&&(days<=120))
+			res=sum+(sum/100*2);
+		if((days>=121)&&(days<=240))
+			res=sum+(sum/100*6);
+		if((days>=241)&&(days<=365))
+			res=sum+(sum/100*12);			
+	}
 
 }
