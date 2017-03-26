b1432bfde70a11d3a02e96e446884a9fd882d4bf
Arthur Sergeev <sergeevski@outlook.com>
2017-03-27 03:03:27 +0700

    Changed function main

--- Lab2.c
+++ Lab2.c
@@ -1,44 +1,17 @@
 #include <stdio.h>
+#include "Deposit.h"
 
 int main()
-{
-	int sum, days, res;
-	printf("Insert sum, please: ");
-	scanf("%d", &sum);
-	printf("\nInsert days, please: ");
-	scanf("%d", &days);
-	while(sum<10000)
-	{
-		printf("\nFailed! Insert please sum not less than 10,000: ");
-		scanf("%d", &sum );
-	}
-	while(days>365)
-	{
-		printf("\nFailed! Insert please days no more than 365: ");
-		scanf("%d", &days );
-	}
-	if(sum<=100000)
-	{
-		if((days>=0)&&(days<=30))
-			res=sum-(sum/100*10);
-		if((days>=31)&&(days<=120))
-			res=sum+(sum/100*2);
-		if((days>=121)&&(days<=240))
-			res=sum+(sum/100*6);
-		if((days>=241)&&(days<=365))
-			res=sum+(sum/100*12);			
-	}
-	if(sum>100000)
-	{
-		if((days>=0)&&(days<=30))
-			res=sum-(sum/100*10);
-		if((days>=31)&&(days<=120))
-			res=sum+(sum/100*3);
-		if((days>=121)&&(days<=240))
-			res=sum+(sum/100*8);
-		if((days>=241)&&(days<=365))
-			res=sum+(sum/100*15);			
-	}
-	printf("\nResult will be: %d", res);
-
+{	
+	Deposit *dep;
+	dep = int_dep_new(5);
+	printf("Enter sum, please: ");
+	scanf("%zu", &dep->sum);
+	printf("\n");
+	printf("Enter days, please: ");
+        scanf("%zu", &dep->days);
+        printf("\n");
+	data_checking(dep);
+        calculation_of_contribution(dep);
+	return 0;
 }
